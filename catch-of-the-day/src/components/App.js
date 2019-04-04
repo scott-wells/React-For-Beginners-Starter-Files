be8884ea7ed4fcea282900271fcf8fd90ff5d6ad
Scott Wells <efwells101@gmail.com>
2019-04-04 16:52:38 -0600

    create fish form and addFish method and pass it down via props; also created an object loader and passed it down via props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,24 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+       // 1. Take a copy of an existing state
+       const fishes = {...this.state.fishes};
+       // 2. Add our new fish to the fishes variable
+       fishes[`fish${Date.now()}`] = fish;
+       // 3. Set the new fishes object to state
+       this.setState({ fishes });
+    }
+
+    loadSampleFishes = () => {
+        alert('Loading Samples');
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +30,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish } loadSampleFishes={this.loadSampleFishes} />
             </div>  
         )
     }
