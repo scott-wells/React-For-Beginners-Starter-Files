eae6de01617bbc66740ce3b9ed763cacc80ad9be
Scott Wells <efwells101@gmail.com>
2019-04-04 14:16:08 -0600

    when the store button is pressed, the url goes to the store with that current value; add fish form created

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,11 +3,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef(); 
+
+    goToStore = event => {
+        // 1. Stop form from submitting
+        event.preventDefault();
+        // 2. Get the text from that input
+        const storeName = this.myInput.current.value;
+        // 3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()} 
+                />
                 <button type="submit">Visit Store -></button>
             </form>
         )
